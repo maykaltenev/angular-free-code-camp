2c1ad040a7e8e98134074a7ac27adf5fba242080
Maykal Tenev <michaeltenev@gmail.com>
2023-05-20 23:13:26 +0200

    multiply

--- typescript/functions.ts
+++ typescript/functions.ts
@@ -4,3 +4,7 @@ function add(num1: number, num2: number) {
 console.log(add(2, 3));
 
 const sub = (num1: number, num2: number): number => num1 - num2;
+
+const mult = function (num1: number, num2: number): number {
+  return num1 - num2;
+};
