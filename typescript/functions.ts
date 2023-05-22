0d67b88a72ee33f336d2fd63cc9048cd1023258d
Maykal Tenev <michaeltenev@gmail.com>
2023-05-22 23:28:56 +0200

    optional parameter

--- typescript/functions.ts
+++ typescript/functions.ts
@@ -1,10 +1,10 @@
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number, num2: number, num3?: number) {
+  return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(2, 3));
 
 const sub = (num1: number, num2: number): number => num1 - num2;
 
-const mult = function (num1: number, num2: number): number {
+const multiply = function (num1: number, num2: number): number {
   return num1 - num2;
 };
