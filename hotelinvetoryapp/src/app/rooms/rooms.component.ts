effbeb35f45a7df944d42f5f887f1c26c10665b0
Maykal Tenev <michaeltenev@gmail.com>
2023-06-13 00:26:46 +0200

    ngFor

--- hotelinvetoryapp/src/app/rooms/rooms.component.ts
+++ hotelinvetoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -9,6 +10,43 @@ export class RoomsComponent {
   hotelName = 'My hotel';
   numberOfRooms = 10;
   hideRooms = false;
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 120,
+    bookedRooms: 5,
+  };
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner',
+      price: 500,
+      photos:
+        'https://tse4.mm.bing.net/th?id=OIP.t-SKlKn52jfSnq-cUiAGQgHaFy&pid=Api',
+      checkinTime: new Date(),
+      checkoutTime: new Date(),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Nice Room',
+      amenities: 'Air Conditioner',
+      price: 5200,
+      photos:
+        'https://tse4.mm.bing.net/th?id=OIP.t-SKlKn52jfSnq-cUiAGQgHaFy&pid=Api',
+      checkinTime: new Date(),
+      checkoutTime: new Date(),
+    },
+    {
+      roomNumber: 3,
+      roomType: 'Private Room',
+      amenities: 'Air Conditioner',
+      price: 1500,
+      photos:
+        'https://tse4.mm.bing.net/th?id=OIP.t-SKlKn52jfSnq-cUiAGQgHaFy&pid=Api',
+      checkinTime: new Date(),
+      checkoutTime: new Date(),
+    },
+  ];
   toggle() {
     this.hideRooms = !this.hideRooms;
   }
